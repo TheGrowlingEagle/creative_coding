6da20eaf81bf57fd3a6d74f5b73beba609cdef73
TheGrowlingEagle <Zachariah.Radke@gmail.com>
2021-07-22 11:12:51 +1000

    added code

--- Assessment 1/sketch.js
+++ Assessment 1/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
   createCanvas(400, 400);
+  frameRate(60)
 }
 
 function draw() {
-  background(220);
+  background(100, 200, 250);
+  ellipse(mouseX, mouseY, 30, 30);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  }else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
